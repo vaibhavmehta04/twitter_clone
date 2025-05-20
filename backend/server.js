6abd63b4c0a508c28afb65411e7cc16916ea45e1
Vaibhav Mehta <mehtapiyu11@gmail.com>
2025-05-20 20:23:48 +0530

    tr

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use('/api/posts', postRoutes);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(Path.join(__dirname, '/frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    res.sendFile(Path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   })
 }
 
