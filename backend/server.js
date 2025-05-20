3eb6bddfdb4d30498cfe85a3b9aa8a095890fb16
Vaibhav Mehta <mehtapiyu11@gmail.com>
2025-05-20 19:09:28 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,11 @@
 
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cookieParser from 'cookie-parser';
-// import {v2 as cloudinary} from 'cloudinary'; 
-// dotenv.config();
-
-
-// import authRoutes from './routes/auth.routes.js';
-// import userRoutes from './routes/user.routes.js'; 
-// import postRoutes from './routes/post.routes.js';
-// import connectMongoDb from './db/connectMongoDb.js';
-
-
-//  cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,});
-// const app=express();
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
-// app.use(cookieParser());
-
-
-// app.use("/api/auth",authRoutes);
-// app.use("/api/users",userRoutes);
-// app.use("/api/posts",postRoutes);
- 
-
-// app.listen(8000,()=>{
-//     console.log("server started on port 8000");
-//     connectMongoDb();
-// });
 
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary';
 import cors from 'cors'; // <--- ADD THIS
+import Path from 'path';
 
 dotenv.config();
 
@@ -51,6 +20,8 @@ const corsOptions = {
   credentials: true, // allow credentials like cookies
 };
 const app = express();
+const __dirname = Path.resolve();
+
 app.use(cors(corsOptions)); // <--- ADD THIS
 
 cloudinary.config({
@@ -66,6 +37,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(Path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  })
+}
 
 app.listen(8000, () => {
   console.log('server started on port 8000');
